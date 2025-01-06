f718f1efaefb71ed96219df84894906e2bd6a7a0
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-01-06 00:28:14 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here!
+const list = document.querySelector('#infi-list');
+
+function addListItems(count){
+	for(i=0;i<count;i++){
+		const listItem = document.createElement('li');
+		listItem.textcontent =`Item ${list.children.length+1}`;
+		list.appendChild(listItem);
+	}
+}
+addListItems(10);
+
+window.addEventListener('scroll',() =>{
+	if(window.innerHeight+window.scrollY >=document.documentElement.offsetHeight){
+		addListItems(2);
+	}
+});
 
